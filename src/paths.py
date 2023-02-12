9ae41858758cde4d5c72a7f527aae1653f83e5c1
Sonter <108224581+S0nter@users.noreply.github.com>
2023-02-12 14:35:51 +0000

    Delete src directory

--- src/paths.py
+++
@@ -1,6 +0,0 @@
-from os import getcwd, path
-base_path = getcwd()
-stat_path = path.join(base_path, 'Statistic')
-sett_path = path.join(base_path, 'sett.slset')
-log_path = path.join(base_path, 'logs.txt')
-tran_path = path.join(base_path, 'Translations')
