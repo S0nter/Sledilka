0759b362e106ad2af9224727a625a499dee5bf5f
Sonter <108224581+S0nter@users.noreply.github.com>
2023-06-20 09:58:29 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ python3 setup.py build sdist
 if [ -z "$1" ];  then
     python3 -m pip install dist/*
 else
-    python3 -m pip install dist/* --target="$1"
+    python3 -m pip install dist/* --prefix="$1"
 fi 
