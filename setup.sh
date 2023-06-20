a58e0ea0153baebec7479b9c0fdd666428205c55
Sonter <108224581+S0nter@users.noreply.github.com>
2023-06-20 09:26:33 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-  then
-    prefix=$HOME/.local
-else
-    prefix=$1
-fi
-# pip uninstall Sledilka
 python3 setup.py build sdist
-python3 -m pip install dist/* --target=$prefix
+
+if [ -z "$1" ];  then
+    python3 -m pip install dist/*
+else
+    python3 -m pip install dist/* --target="$1"
+fi 
